5a966447c813b93f57c4a88abc40fa6df14cc7d5
unknown <E460474@ZFC248.inetpsa.com>
2017-05-05 10:41:55 +0800

    remove springBootMcv

--- springBootMvc/run.bat
+++
@@ -1,1 +0,0 @@
-java -jar target/springBootMvc-1.0-SNAPSHOT.jar
--- springBootMvc/src/main/java/com/boot/service/SubMenuService.java
+++
@@ -1,7 +0,0 @@
-package main.java.com.boot.service;
-
-/**
- * Created by hao huang on 13/07/2016.
- */
-public interface SubMenuService {
-}
