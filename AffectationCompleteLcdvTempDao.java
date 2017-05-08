48e0e5e5dd05d96b7ea42470a53ce45ec8a6a263
HaultHuang <E460474@ZFC248.inetpsa.com>
2017-05-08 10:19:40 +0800

    Revert "add hello.txt"
    This reverts commit 01706d8d1975d651a530c4758f94782fb6793728.
    
    Conflicts:
    	hello.txt

---
+++ AffectationCompleteLcdvTempDao.java
@@ -0,0 +1,48 @@
+/*
+ * Creation : 12 Jan 2017
+ */
+package com.inetpsa.rvr.db.dao;
+
+import java.util.Collection;
+import java.util.Date;
+
+import com.inetpsa.rvr.common.exception.SfjDaoException;
+import com.inetpsa.rvr.db.model.AffectationCompleteLcdvTemp;
+
+/**
+ * The Interface AffectationCompleteLcdvTempDao.
+ */
+public interface AffectationCompleteLcdvTempDao extends GenericDao<AffectationCompleteLcdvTemp, Long> {
+
+    /**
+     * Creates the temp table.
+     * 
+     * @throws SfjDaoException the sfj dao exception
+     */
+    void createTable() throws SfjDaoException;
+
+    /**
+     * Drop table.
+     * 
+     * @throws SfjDaoException the sfj dao exception
+     */
+    void dropTable() throws SfjDaoException;
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see com.inetpsa.rvr.db.dao.GenericDao#create(java.lang.Object)
+     */
+    AffectationCompleteLcdvTemp create(AffectationCompleteLcdvTemp entiy) throws SfjDaoException;
+
+    /**
+     * Find by version.
+     *
+     * @param idVersion the id version
+     * @param dateStart the date start
+     * @param dateEnd the date end
+     * @return the collection
+     * @throws SfjDaoException the sfj dao exception
+     */
+    Collection<AffectationCompleteLcdvTemp> findByVersion(Long idVersion, Date dateStart, Date dateEnd) throws SfjDaoException;
+}
